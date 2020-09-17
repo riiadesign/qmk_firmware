3616abf1d3b036c0d07268bbc69c3e8b867b2a2c
Adam Gwilliam <agwilliam@yaharasoftware.com>
2020-09-17 15:10:15 -0500

    make lower and raise both be layer taps along with space and enter

--- keyboards/preonic/keymaps/gwillad/keymap.c
+++ keyboards/preonic/keymaps/gwillad/keymap.c
@@ -56,7 +56,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, \
   KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, \
-  KC_CAPS, BACKLIT, KC_LGUI, KC_LALT, LOWER,   KC_SPC,  KC_ENT,  RAISE,   KC_RALT, KC_RCTL, MACRO1,   KC_DEL   \
+  KC_CAPS, BACKLIT, KC_LGUI, KC_LALT, LT(_LOWER, KC_SPC),   LT(_LOWER, KC_SPC),  LT(_RAISE, KC_ENT),  LT(_RAISE, KC_ENT),   KC_RALT, KC_RCTL, MACRO1,   KC_DEL   \
 ),
 
 /* Colemak
