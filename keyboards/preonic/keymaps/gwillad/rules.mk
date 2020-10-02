5b4e03fdaddf24d1ba8b0e60c559bc69cc8e5616
Adam Gwilliam <agwilliam@yaharasoftware.com>
2020-10-02 15:11:31 -0500

    clean up a bunch of code

--- keyboards/preonic/keymaps/gwillad/rules.mk
+++ keyboards/preonic/keymaps/gwillad/rules.mk
@@ -1,1 +1,12 @@
 SRC += muse.c
+RGBLIGHT_ENABLE = no        # Enable WS2812 RGB underlight.
+AUDIO_ENABLE = no           # Audio output
+
+MOUSEKEY_ENABLE = no      # Mouse keys
+EXTRAKEY_ENABLE = no       # Audio control and System control
+CONSOLE_ENABLE = no         # Console for debug
+COMMAND_ENABLE = no       # Commands for debug and configuration
+
+
+ENCODER_ENABLE = no
+DIP_SWITCH_ENABLE = no
