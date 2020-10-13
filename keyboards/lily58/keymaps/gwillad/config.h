b5f229ff2af701104d859501540630bb6398c2f7
Adam Gwilliam <agwilliam@yaharasoftware.com>
2020-10-13 09:14:05 -0500

    remove unused defs, (and get rid of TAPPING_FORCE_HOLD), update keymap slightly

--- keyboards/lily58/keymaps/gwillad/config.h
+++ keyboards/lily58/keymaps/gwillad/config.h
@@ -32,21 +32,4 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define USE_SERIAL_PD2
 
-#define TAPPING_FORCE_HOLD
 #define TAPPING_TERM 200
-
-#undef RGBLED_NUM
-#define RGBLIGHT_ANIMATIONS
-#define RGBLED_NUM 27
-#define RGBLIGHT_LIMIT_VAL 120
-#define RGBLIGHT_HUE_STEP 10
-#define RGBLIGHT_SAT_STEP 17
-#define RGBLIGHT_VAL_STEP 17
-
-// Underglow
-/*
-#undef RGBLED_NUM
-#define RGBLED_NUM 14    // Number of LEDs
-#define RGBLIGHT_ANIMATIONS
-#define RGBLIGHT_SLEEP
-*/
