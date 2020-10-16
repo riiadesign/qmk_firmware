2a0f4d6eec8baaf1b2f5904b8d982682ed051e09
Adam Gwilliam <agwilliam@yaharasoftware.com>
2020-10-16 10:20:54 -0500

    plankc'd the preonic

--- keyboards/preonic/keymaps/gwillad/keymap.c
+++ keyboards/preonic/keymaps/gwillad/keymap.c
@@ -18,56 +18,58 @@
 #include "muse.h"
 
 enum preonic_layers {
-  _QWERTY,
+  _DEFAULT,
   _LOWER,
   _RAISE,
+  _ADJUST,
 };
 
 enum preonic_keycodes {
   LAUNCHY  = SAFE_RANGE,
   CMDER,
+  YAHARA,
+  YSFTWRE,
+  YEMAIL,
+  GMAIL
 };
 
 #define SPCLOWER LT(_LOWER, KC_SPC)
 #define ENTRAISE LT(_RAISE, KC_ENT)
+#define CTLESC   MT(MOD_LCTL, KC_ESC)
+#define SFTDEL   MT(MOD_RSFT, KC_DEL)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-/* Qwerty
- * ,-----------------------------------------------------------------------------------.
- * | Esc  |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  |   \  |
- * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Tab  |   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  | Bksp |
- * |------+------+------+------+------+-------------+------+------+------+------+------|
- * | Ctrl |   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   ;  |  "   |
- * |------+------+------+------+------+------|------+------+------+------+------+------|
- * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |Shift |
- * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Caps |LAUNCH| GUI  | Alt  |  Lower Spc  |  Ent Raise  | Alt  | Ctrl | CMDER| DEL  |
- * `-----------------------------------------------------------------------------------'
- */
-[_QWERTY] = LAYOUT_preonic_grid( \
-  KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,     KC_5,     KC_6,     KC_7,     KC_8,    KC_9,    KC_0,    KC_GRV, \
-  KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,    KC_O,    KC_P,    KC_BSPC, \
-  KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,     KC_G,     KC_H,     KC_J,     KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
-  KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, \
-  KC_CAPS, LAUNCHY, KC_LGUI, KC_LALT, SPCLOWER, SPCLOWER, ENTRAISE, ENTRAISE, KC_RALT, KC_RCTL, CMDER,   KC_DEL   \
+[_DEFAULT] = LAYOUT_preonic_grid(
+  KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,   KC_NO,   KC_NO,   KC_NO,
+  KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,    KC_O,    KC_P,    KC_BSPC,
+  CTLESC,  KC_A,    KC_S,    KC_D,    KC_F,     KC_G,     KC_H,     KC_J,     KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+  KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM, KC_DOT,  KC_SLSH, SFTDEL,
+  KC_CAPS, LAUNCHY, KC_LGUI, KC_LALT, SPCLOWER, SPCLOWER, ENTRAISE, ENTRAISE, KC_RALT, KC_RCTL, CMDER,   KC_DEL
 ),
 
-[_LOWER] = LAYOUT_preonic_grid( \
-  _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_BSLS,  \
-  _______, _______, _______, _______, _______, _______, _______, KC_LBRC, KC_RBRC, KC_MINS, KC_EQL,  _______,  \
-  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
-  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______, _______, \
-  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______  \
+[_LOWER] = LAYOUT_preonic_grid(
+  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+  _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______,
+  _______, _______, _______, _______, _______, _______, _______, KC_LBRC, KC_RBRC, KC_MINS, KC_EQL,  _______,
+  _______, _______, _______, _______, _______, _______, _______, KC_GRV, KC_BSLS, _______,  _______, _______,
+  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
 ),
 
-[_RAISE] = LAYOUT_preonic_grid( \
-  _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_F11,  KC_F12,  KC_BSLS,  \
-  _______, _______, _______, _______, _______, _______, _______, _______, KC_UP,   _______, _______,  _______,  \
-  _______, _______, _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, \
-  _______, _______, _______, _______, _______, _______, _______, _______, KC_HOME, KC_END,  _______, _______, \
-  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______  \
+[_RAISE] = LAYOUT_preonic_grid(
+  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+  _______, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______,
+  _______, YAHARA,  YSFTWRE, YEMAIL,  GMAIL,   _______, _______, _______, KC_UP,   _______, _______, _______,
+  _______, _______, _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_RGHT, KC_HOME, KC_END,
+  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
+),
+
+[_ADJUST] = LAYOUT_preonic_grid(
+  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
+  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
 )
 };
 
@@ -83,6 +85,30 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
                 SEND_STRING(SS_LSFT(SS_RSFT("`")));
             }
             break;
+        case YAHARA:
+            if (record->event.pressed) {
+                SEND_STRING("yahara");
+            }
+            break;
+        case YSFTWRE:
+            if (record->event.pressed) {
+                SEND_STRING("yaharasoftware");
+            }
+            break;
+        case YEMAIL:
+            if (record->event.pressed) {
+                SEND_STRING("agwilliam@yaharasoftware.com");
+            }
+            break;
+        case GMAIL:
+            if (record->event.pressed) {
+                SEND_STRING("adamgwilliam@gmail.com");
+            }
+            break;
         }
     return true;
 };
+
+layer_state_t layer_state_set_user(layer_state_t state) {
+  return update_tri_layer_state(state, _LOWER, _RAISE, _ADJUST);
+}
